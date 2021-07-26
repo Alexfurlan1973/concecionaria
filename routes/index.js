394d7519025b422fda15a997bc62001d1f555734
Alexandre Furlan <alexfurlan1973@gmail.com>
2021-07-26 16:27:49 -0300

    Fazendo menu hamburguer.

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const home = require('../controllers/home');
 
-/* GET home page. */
 router.get('/', home.home);
-/*router.get('/home', home.home);*/
 router.get('/carros/:id', home.exibeVeiculo);
 
 module.exports = router;
