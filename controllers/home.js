5605b4593e084c476bb081c137b335e37f1a1bdb
Alexandre Furlan <alexfurlan1973@gmail.com>
2021-07-26 16:43:18 -0300

    Arrumando a pagina index.ejs.

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,54 @@ const { UnorderedCollection } = require("http-errors");
 
 module.exports.index = (req, res) => {
     res.render('index', {
+        carros: [{
+            id: 1,
+            name: 'Uno 1.6R',
+            descricao: 'loren',
+            principal: {
+                id:2,
+                caminho: '/img/car-header.png'
+            }
+        },
+        {
+            id: 2,
+            name: 'Camaro V8 SS',
+            descricao: 'loren'
+        },
+        {
+            id: 3,
+            name: 'Ka 1.0',
+            descricao: 'loren'
+        },
+        {
+            id: 4,
+            name: 'Chevette 1.6 SL',
+            descricao: 'loren'
+        },{
+            id: 5,
+            name: 'City LX',
+            descricao: 'loren'
+        },
+        {
+            id: 6,
+            name: 'Golf 2.0 MI Flex Comfortline Sport',
+            descricao: 'loren'
+        },
+        {
+            id: 7,
+            name: 'Fusca 67',
+            descricao: 'loren'
+        },
+        {
+            id: 8,
+            name: 'Saturn SC 1.9 Cupê',
+            descricao: 'loren'
+        },
+        {
+            id: 9,
+            name: 'Clio Hatch Expression',
+            descricao: 'loren'
+        },],
         title: 'Loja de Carros',
         pagina: 'index'
     });
