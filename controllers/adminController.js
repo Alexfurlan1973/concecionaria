babaef8036c23c81de4079c8677e5b52082219fb
Alexandre Furlan <alexfurlan1973@gmail.com>
2021-08-15 21:19:15 -0300

    Deletando arquivos desnecessarios.

--- controllers/adminController.js
+++
@@ -1,7 +0,0 @@
-const models = require('../models')
-
-module.exports.criarAdmin = (async (req, res) => {
-    const admin = req.body
-    await models.Administrador.create(admin)
-    res.send(201)
-})
